b1a4ca72d002d93a21cf0fbf2d5061120d2d38ab
ADMIN <ADMIN@DESKTOP-P7VDQ3I>
2025-01-15 00:11:08 +0200

    E2E test class skeleton committed..

--- APITestProject_Avi/E2E_Tests/E2E_Tests.cs
+++ APITestProject_Avi/E2E_Tests/E2E_Tests.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using APITestProject_Avi.DTOs;
+using APITestProject_Avi.Utility;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +12,69 @@ namespace APITestProject_Avi.E2E_Tests
 {
     public class E2E_Tests
     {
+        #region E2E Scenarios
 
+        /// <summary>
+        /// This end-to-end test verifies the consecutive deposit and withdrawal actions
+        /// </summary>
+        /// <returns>Task</returns>
+        [Test]
+        public async Task Verify_DepositAndWithdrawAmounts()
+        {
+            try
+            {
+                // get balance
+                // deposite 600
+                // get balance
+                // withdraw 150
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /// <summary>
+        /// This end-to-end test verifies the withdraw of amount greater than balance
+        /// </summary>
+        /// <returns>Task</returns>
+        [Test]
+        public async Task Verify_WithdrawOfAmountGreaterThanBalance()
+        {
+            try
+            {
+                // get balance
+                // Try to withdraw amount = balance+10
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        /// <summary>
+        /// This end-to-end test verifies the withdraw of amount greater than balance
+        /// </summary>
+        /// <returns>Task</returns>
+        [Test]
+        public async Task Verify_TryToWithdrawAmountWhenBalanceIsZero()
+        {
+            try
+            {
+                // get balance
+                // Withdraw amount = balance
+                // get balance
+                // Try to withdraw 60
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        #endregion
     }
 }
